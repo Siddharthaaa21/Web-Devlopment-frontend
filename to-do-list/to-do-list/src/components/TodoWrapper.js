05a7b380d8d50f3285d2c71065cb7b2efe705418
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-17 13:31:07 +0530

    added css style and editing todo wrapper

--- to-do-list/to-do-list/src/components/TodoWrapper.js
+++ to-do-list/to-do-list/src/components/TodoWrapper.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {TodoForm} from './TodoForm';
 export const TodoWrapper =()=>{
-    const addTodo = text =>{
+    const [todos, setTodos]=useState([])
+    const addTodo = todo =>{
+        
+            setTodo([...todos,{id: uuidv3(), task: todo,
+            completed: false, isEditing: false}])
+
+        }
+        // this is the function which will add the todo to the list
     return (
         <div className='TodoWrapper'>
 

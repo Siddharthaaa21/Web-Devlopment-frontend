ed864c22ed17459e2a81e716e6971b97d691d76a
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-17 16:30:00 +0530

    fixed

--- to-do-list/to-do-list/src/components/TodoWrapper.js
+++ to-do-list/to-do-list/src/components/TodoWrapper.js
@@ -5,19 +5,19 @@ import {v4 as uuidv4} from 'uuid';
 uuidv4();//initialising and calling it.//same thing they are 
 
 export const TodoWrapper =()=>{
-    const [todos, setTodos]=useState([])
+    const [todos, setTodo]=useState([])
     //use state is used for state management to add react hooks well have to do 
     
     const addTodo = todo =>{
         // is a function  addTodo which takes todo as a parameter
             setTodo([...todos,
-            {id: uuidv3(), task: todo,
+            {id: uuidv4(), task: todo,
                 // since we need to set todos andadding ids which is called uuidv4 and the taks is to todo
                 // passing in by default completed is false and is editing is false 
-            completed: false, isEditing: false}])
+            completed: false, isEditing: false}]);
             console.log(todos)
 
-        }
+        };
         // this is the function which will add the todo to the list
     return (
         <div className='TodoWrapper'>
@@ -28,5 +28,5 @@ export const TodoWrapper =()=>{
             {/* add todo is a funciton well create here  */}
         </div>
         
-    )
-}
+    );
+};

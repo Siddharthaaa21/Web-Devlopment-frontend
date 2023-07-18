f390b5bc532524845cf3b5ac92d24b53eac2a85b
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-18 22:17:15 +0530

    video part 6 completed with comments

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// function Greet() {
+//     return <h1>Hello Siddhartha</h1>
+// }
+// export default Greet
+//  first funtional component 
+//prefered way to write functional component is arrow function
+const Greet=()=> <h1>Hello Siddhartha</h1>
+export default Greet
+// we can change name inside qpp.js from Greet tp any other in the import and custom funtion tag
+//can add export default Greet in the function itself like 
+// export const Greet=()=> <h1>Hello Siddhartha</h1>

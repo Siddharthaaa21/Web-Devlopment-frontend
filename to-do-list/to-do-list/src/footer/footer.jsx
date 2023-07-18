b03ade7c9d6f7b4749e46d59fc401353121bf25d
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-18 15:54:06 +0530

    added hello world in project

---
+++ to-do-list/to-do-list/src/footer/footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="sb__footer section__padding">
+                <div className="sb__footer-links">
+                <div className="sb__footer-links-div">
+                    <h4>For bussiness </h4>
+                    <a href="id">Contact us
+                    {/* <a href="id">About us</a> attach your portfolio */}
+                    <p>Employe</p></a>
+                    <a href="id">Contact us
+                    <p>somethjing</p></a>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    
+</div>
+<div className="sb__footer-links-div"> 
+                    <h4>reasorces </h4>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    </div>
+                    <div className="sb__footer-links-div">
+                    <h4>For bussiness </h4>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    <a href="id">Contact us
+                    <p>others</p></a>
+                    </div>
+                   
+
+
+</div>
+</div>
+</div>
+
+
+
+    )
+}
+export default Footer;

5a7e099a68919f60e4654e4ae07929fdfecade72
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-23 23:41:39 +0530

    added 14th video content

--- learning/hello-world/src/components/Eventbind.js
+++ learning/hello-world/src/components/Eventbind.js
@@ -18,9 +18,11 @@ import React, { Component } from 'react'
     return (
       <div>
         {this.state.message}
-        <button ocClick={this.clickHandler.bind(this)}>click</button>
+        <button onClick={this.clickHandler.bind(this)}>click</button>
+
         {/* first approach to uses binfing methode. */}
 {/* second apporach uses arrow function body
+bind methode is the third approach which is the best approach and usedquite often. also the bind is the best approach for passing the parameter to the event handler. 
 <button onClick={()=> this.clickhander()}><button/> */}
       </div>
     )

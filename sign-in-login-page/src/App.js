5ae6bc3088f8e2e724a85d975c6d7287a84891b7
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-28 15:36:37 +0530

    initial commit

--- sign-in-login-page/src/App.js
+++ sign-in-login-page/src/App.js
@@ -1,12 +1,12 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Sign_in from './componets/Sign-in/Sign_in';
+import Sign_in from './Components/Sign_in';
+
 
 function App() {
   return (
     <div className="App">
       <Sign_in/>
-      
     </div>
   );
 }
--- sign-in-login-page/src/Components/Sign_in.js
+++ sign-in-login-page/src/Components/Sign_in.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './Sign_in.css';
-import { useForm } from 'react-hooks-form';
+
 
 export default function Sign_in() {
   return (

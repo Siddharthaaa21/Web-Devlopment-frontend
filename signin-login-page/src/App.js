1c855dafb0dfa9bc1c547447299c584528451a53
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-07-28 14:38:38 +0530

    updating packages

--- signin-login-page/src/App.js
+++ signin-login-page/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Sign_in from './componets/Sign-in/Sign_in';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sign_in/>
+      
     </div>
   );
 }

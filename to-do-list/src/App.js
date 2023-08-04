2b28a0ddd46c411980eaf11bd576d3c88e79c888
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-04 23:52:53 +0530

    ADED

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {TodoWrapper} from './components/TodoWrapper'
-
+import './footer/footer'
 function App() {
   return (
     <div className="App">
       <TodoWrapper />
+      <footer/>
+    
     
      
     </div>

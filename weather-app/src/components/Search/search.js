fb7a3674525ce526621ab84299ffa31b03bae455
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-10 01:27:46 +0530

    day 1 of weather app added packages

---
+++ weather-app/src/components/Search/search.js
@@ -0,0 +1,11 @@
+//component used in pakages we installed 
+import React from 'react'
+
+ const Search=()=> {
+  return (
+    <div className="container">
+      search
+    </div>
+  )
+}
+export default Search;

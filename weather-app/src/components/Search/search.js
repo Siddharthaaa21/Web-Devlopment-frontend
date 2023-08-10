c6ee3dba3f815ef1319164b2b740e20186ff2143
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-11 00:33:00 +0530

    added further work on react weather app

--- weather-app/src/components/Search/search.js
+++ weather-app/src/components/Search/search.js
@@ -1,11 +1,21 @@
 //component used in pakages we installed 
-import React from 'react'
+import React, { useState } from 'react'
+import {AsyncPaginate} from "react-select-async-paginate"
 
  const Search=()=> {
+  const [search, setSearch]=useState(null); 
+  const handleOnChange={seacrhData}=>{
+    setSearch(seacrhData); 
+  }
+
   return (
-    <div className="container">
-      search
-    </div>
+    // AsyncPaginate is a term used for loadinf data in pages asyncronously means data is fetched and displayed without blocking user interface 
+    //could change the appereacne props and hooks 
+    <AsyncPaginate  
+    placeholder="Search For City"
+    debounceTimeout={600}
+    value={search}
+    onChange={handleOnChange}/>
   )
 }
 export default Search;

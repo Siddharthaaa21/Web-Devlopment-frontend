1288f02e8bbc59be3b3524ee0327877e43c04364
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-16 00:06:55 +0530

    added images

--- weather-app/src/components/currrent-weather/current_weather.js
+++ weather-app/src/components/currrent-weather/current_weather.js
@@ -3,8 +3,12 @@ import "./current_weather.css"
 
 const Current=()=> {
   return (
-    <div>
-      hello
+    <div className="weather-one">
+      <div className="upper">
+        <p className="city">delhi</p>
+        <p className="weather-description">rain</p>
+      </div>
+      <img alt="weather" className="weather-icon" src="icons/01d.png"/>
     </div>
   )
 }

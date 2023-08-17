98d68ea27fef05abae9826345d69468991252db2
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-17 23:45:34 +0530

    working on css

--- weather-app/src/components/currrent-weather/current_weather.js
+++ weather-app/src/components/currrent-weather/current_weather.js
@@ -5,8 +5,11 @@ const Current=()=> {
   return (
     <div className="weather-one">
       <div className="upper">
+       <div>
+
         <p className="city">delhi</p>
         <p className="weather-description">sunny</p>
+        </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png"/>
 
       </div>

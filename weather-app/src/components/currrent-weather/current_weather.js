77118125ace066d49f16e351711287e1b7f19758
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-21 20:08:28 +0530

    weather app work

--- weather-app/src/components/currrent-weather/current_weather.js
+++ weather-app/src/components/currrent-weather/current_weather.js
@@ -1,19 +1,53 @@
 import "./current_weather.css"
 
 
-const Current=()=> {
+const Current = () => {
   return (
     <div className="weather-one">
       <div className="upper">
-       <div>
 
-        <p className="city">delhi</p>
-        <p className="weather-description">sunny</p>
+        <div>
+          <p className="city"> delhi </p>
+          <p className="weather-description"> sunny </p>
+
+
+
+          {/* <p className="weather-description">sunny</p> */}
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+
 
       </div>
-    </div>
+      <div className="bottom">
+        <p className="temp">18° </p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label"> Details </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">feels like </span>
+            <span className="parameter-value">  22° </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label"> wind</span>
+            <span className="parameter-value">  2 m/s </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">humidity </span>
+            <span className="parameter-value">  50 percent </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">pressure </span>
+            <span className="parameter-value">  15 hPa </span>
+          </div>
+
+
+
+        </div>
+      </div>
+
+
+    </div >
   )
 }
 

21146c72e8400aef9cc29c9db068922dd8f4e247
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-21 20:14:04 +0530

    done for day 3

--- weather-app/src/components/currrent-weather/current_weather.js
+++ weather-app/src/components/currrent-weather/current_weather.js
@@ -22,7 +22,7 @@ const Current = () => {
         <p className="temp">18° </p>
         <div className="details">
           <div className="parameter-row">
-            <span className="parameter-label"> Details </span>
+            <span className="parameter-label top"> Details </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">feels like </span>

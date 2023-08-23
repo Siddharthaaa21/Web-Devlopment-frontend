5a13a20d68ea93748eee73933b7c9bb9fb35eefc
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-23 11:32:26 +0530

    changing the folder structure

--- sign-in-login-page/src/App.js
+++ sign-in-login-page/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import SignIn from './Components/Sign_in';
+import SignIn from './Components/Sign in/Sign_in'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignUp from './Components/Sign Up/SignUp';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <SignIn/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/Sign_in" component={SignIn} />
+        <Route path="/SignUp" component={SignUp}/>
+      </Switch>
+    </Router>
   );
 }
 

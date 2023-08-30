4ad1138029fee63b587a131c9cce14888cd5fb87
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-08-31 00:35:03 +0530

    added api keys

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Search from './components/Search/search';
 import Current, { current_weather } from './components/currrent-weather/current_weather';
-
+import { Weather_API_URL } from './api';
+import { Weather_API_KEY } from './api';
 function App() {
   const handleOnSeacrchChange=(data_entered)=>{
-    console.log(data_entered)
+   const[lat,lon]= data_entered.value.split(" ");
+   //fetching the va;ues (2)
+// eslint-disable-next-line no-template-curly-in-string
+const current_weatherFeth  = fetch(`${Weather_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${Weather_API_KEY}`)
+const forecastFetch  = fetch(`${Weather_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${Weather_API_KEY}`)
+   Promise.all([current_weatherFeth,forecastFetch])
+   .then(async (response)=>{
+   })
   }
   return (
     <div className="App">
--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -7,7 +7,10 @@
 };
 export const GEO_API_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 
+export const Weather_API_URL='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+export const Weather_API_KEY='a72c5146f0e2909cce3a0770e86cec92';
 // try {
+
 // 	const response = await fetch(url, options);
 // 	const result = await response.text();
 // 	console.log(result);

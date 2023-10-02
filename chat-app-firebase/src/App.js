86c7c7cbfd4d900babdf1f39937eff3a5027c6ff
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2023-10-03 00:08:48 +0530

    fixed the error

--- chat-app-firebase/src/App.js
+++ chat-app-firebase/src/App.js
@@ -1,35 +1,37 @@
 import './App.css';
 import { Auth } from './components/Auth';
 import Cookies from 'universal-cookie'
+
+
+import React, { useState } from 'react';
 const cookies =new Cookies();
 
 
 function App() {
-  const [isAuth,setIsAuth]=useState(cookies.get("auth-token"));
-  const [room,setRoom]=useState(null);
-  if(!isAuth){
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'));
+  const [room, setRoom] = useState(null);
+
+  if (!isAuth) {
     return (
       <div className="App">
-      
-  <Auth/>
-       
+        <Auth />
       </div>
     );
   }
-   return (
+
+  return (
     <div>
-    {room ? (
-      <div >chat </div>
-    ) : (
-      <div className='room'>
-        <label>Room ID:</label>
-        <input/>
-        <button>enter chat</button>
+      {room ? (
+        <div>chat </div>
+      ) : (
+        <div className="room">
+          <label>Room ID:</label>
+          <input />
+          <button>enter chat</button>
         </div>
-    )}
-     
+      )}
     </div>
-   )
+  );
 }
 
 export default App;
